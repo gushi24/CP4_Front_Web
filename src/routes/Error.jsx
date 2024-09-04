02c666f86a6f812519d21d7ad71e2b5129063a38
gushi <mgmorioka@outlook.com>
2024-09-04 17:43:05 -0300

    Fixing css class names -Gushi

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -6,9 +6,11 @@ import homeIcon from '../assets/homeIcon.svg'
 const Error=()=>{
     return (
         <>
-            <h1>404</h1>
-            <h2>UH OH! You're lost.</h2>
-            <p>The page you are looking for does not exist.<br/> You can click the button below to go back to the homepage.</p>
+            <div className='errorText'>
+                <h1>404</h1>
+                <h2>UH OH! You're lost.</h2>
+                <p>The page you are looking for does not exist.<br/> You can click the button below to go back to the homepage.</p>
+            </div>
             <Link>
                 <button className="Btn">
   
