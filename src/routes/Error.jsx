4ad299ac49fe8b2c2c6ee7b3fc5a9b8bd5f2f275
gushi <mgmorioka@outlook.com>
2024-09-07 13:34:46 -0300

    Fixing Logo.jsx and Erro.jsx -Gushi

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -6,23 +6,26 @@ import homeIcon from '../assets/homeIcon.svg'
 const Error=()=>{
     return (
         <>
-            <div className='errorText'>
-                <h1>404</h1>
-                <h2>UH OH! You're lost.</h2>
-                <p>The page you are looking for does not exist.<br/> You can click the button below to go back to the homepage.</p>
+            <div className='errorPage'>
+                <div className='errorText'>
+                    <h1>404</h1>
+                    <h2>UH OH! You're lost.</h2>
+                    <p>The page you are looking for does not exist.<br/> You can click the button below to go back to the homepage.</p>
+                </div>
+                <Link>
+                    <button className="Btn">
+  
+                        <div className="sign">
+                            <img src={homeIcon} alt='icon'></img>
+                        </div>
+  
+                        <div className="text">Home</div>
+                    </button>
+
+
+                </Link>
             </div>
-            <Link>
-                <button className="Btn">
-  
-                    <div className="sign">
-                        <img src={homeIcon} alt='icon'></img>
-                    </div>
-  
-                    <div className="text">Home</div>
-                </button>
 
-
-            </Link>
         </>
     )
 }
